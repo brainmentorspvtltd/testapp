fdd1f0342d028a5e263539d952de09cf108fa608
brainmentorspvtltd <brainmentorspvtltd@gmail.com>
2019-01-17 11:20:38 +0530

    Add files via upload

---
+++ js/db.js
@@ -0,0 +1,13 @@
+
+
+
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyCZjO62Wz9XF2w3E27V_L6hYTFN5pqi2Go",
+    authDomain: "tpddl-83dc4.firebaseapp.com",
+    databaseURL: "https://tpddl-83dc4.firebaseio.com",
+    projectId: "tpddl-83dc4",
+    storageBucket: "tpddl-83dc4.appspot.com",
+    messagingSenderId: "460800001219"
+  };
+  firebase.initializeApp(config);
